0b0154c36a4d7c007ebf71a7e53811ac55671b22
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-08-22 08:53:03 +0000

    Modification de l'index

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,16 +9,28 @@
       <v-col cols="4" class="d-flex justify-center align-end text-center title">
         <p class="text-h6">Aider nos apiculteurs</p>
       </v-col>
-      <v-col v-if="screenWidth < 600" cols="4" class="d-flex justify-end align-end">
+      <v-col
+        v-if="screenWidth < 600"
+        cols="4"
+        class="d-flex justify-end align-end"
+      >
         <v-menu open-on-hover>
           <template v-slot:activator="{ props }">
             <v-btn v-bind="props" class="btnMenu">
-              <img src="@/assets/SVG/Bouton_menu.svg" alt="menu" class="logoMenu" />
+              <img
+                src="@/assets/SVG/Bouton_menu.svg"
+                alt="menu"
+                class="logoMenu"
+              />
             </v-btn>
           </template>
 
           <v-list>
-            <v-list-item v-for="(item, index) in items" :key="index" :value="index">
+            <v-list-item
+              v-for="(item, index) in items"
+              :key="index"
+              :value="index"
+            >
               <v-list-item-title>{{ item.title }}</v-list-item-title>
             </v-list-item>
           </v-list>
@@ -26,48 +38,55 @@
       </v-col>
       <v-col v-else cols="4" class="d-flex align-end">
         <v-row class="d-flex justify-center align-end text-center">
-          <v-col cols="4" class="pa-0" v-for="(item, index) in items" :key="index" :value="index">
-            <router-link class="menuLink" :to="item.link">{{ item.title }}</router-link>
+          <v-col
+            cols="4"
+            class="pa-0"
+            v-for="(item, index) in items"
+            :key="index"
+            :value="index"
+          >
+            <router-link class="menuLink" :to="item.link">{{
+              item.title
+            }}</router-link>
           </v-col>
         </v-row>
       </v-col>
     </v-row>
     <v-row class="d-flex justify-center">
-      <hr class="horizontalLigne">
+      <hr class="horizontalLigne" />
     </v-row>
   </v-container>
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue';
+import { ref, onMounted, onUnmounted } from "vue";
 
 const items = [
-  { title: 'Profil', link: '/' },
-  { title: 'Cathalogue', link: '/' },
-  { title: 'Contact', link: '/' },
-]
+  { title: "Profil", link: "/" },
+  { title: "Cathalogue", link: "/" },
+  { title: "Contact", link: "/" },
+];
 
-const screenWidth = ref(window.innerWidth)
+const screenWidth = ref(window.innerWidth);
 
 function handleResize() {
-  screenWidth.value = window.innerWidth
+  screenWidth.value = window.innerWidth;
 }
 onMounted(() => {
-  window.addEventListener('resize', handleResize)
-})
+  window.addEventListener("resize", handleResize);
+});
 onUnmounted(() => {
-  window.removeEventListener('resize', handleResize)
-})
-
+  window.removeEventListener("resize", handleResize);
+});
 </script>
 
 <style scoped>
 .logo {
-  width: 6em;
+  width: 7em;
 }
 
 .title {
-  color: #5C4725;
+  color: #5c4725;
 }
 
 .logoMenu {
@@ -87,13 +106,13 @@ onUnmounted(() => {
   width: 95%;
   border: none;
   height: 3px;
-  background-color: #5C4725;
+  background-color: #5c4725;
   margin: auto;
 }
 
 .menuLink {
   text-decoration: none;
-  color: #5C4725;
+  color: #5c4725;
   padding: 0;
 }
 
--- src/router/views/index.vue
+++ src/router/views/index.vue
@@ -1,26 +1,123 @@
 <script setup>
-import {onMounted} from 'vue'
-import NavBar from '@/components/NavBar.vue'
+import { onMounted } from "vue";
+import NavBar from "@/components/NavBar.vue";
 
 onMounted(() => {
-    document.title = "BeesPlace"
-})
+  document.title = "BeesPlace";
+});
 </script>
 
 <template>
-    <v-app>
-        <NavBar />
-        <v-main>
-            <v-container>
-                <v-card class="mx-auto" max-width="400">
-                    <v-card-title class="text-h5">
-                        Bienvenu sur le site de BeesPlace
-                    </v-card-title>
-                </v-card>
-            </v-container>
-        </v-main>
-    </v-app>
+  <v-app>
+    <NavBar />
+    <v-main>
+      <v-container>
+        <img
+          class="firstLittleBee"
+          src="@/assets/SVG/Petite_abeille.svg"
+          alt="Image petite abeille"
+        />
+        <img
+          class="secondeLittleBee"
+          src="@/assets/SVG/Petite_abeille.svg"
+          alt="Image petite abeille"
+        />
+        <v-row class="d-flex justify-center">
+          <v-col cols="11">
+            <h2 class="subtitle text-center">
+              <img
+                src="@/assets/SVG/Ruche.svg"
+                alt="Image d'une Ruche"
+                class="imgSubtitle"
+              />
+              Mettez votre terrain aux services des abeilles
+            </h2>
+          </v-col>
+        </v-row>
+        <v-row class="text-center d-flex justify-center">
+          <v-col cols="10">
+            <p>
+              Les abeiles jouent un rôle essentiel dans la pollinisation, mais
+              elles disparaissent à un rythme alarmant.
+            </p>
+          </v-col>
+        </v-row>
+        <v-row class="mt-5">
+          <img
+            src="@/assets/Photos/Photo_fleurs.jpg"
+            alt="Image de fleurs"
+            class="imgFlowers"
+          />
+        </v-row>
+        <v-row class="mt5 d-flex justify-center">
+          <v-col cols="10">
+            <p>
+              Ce site est une passerelle entre les citoyens et les apiculteurs,
+              il permet à chacun de mettre à disposition un terrain même modeste
+              pour accueillir des ruches et soutenir la pollinisation locale.
+            </p>
+          </v-col>
+        </v-row>
+        <v-row class="d-flex justify-end mt-0">
+          <v-col cols="3">
+            <img
+              width="50vw"
+              src="@/assets/SVG/Pot_de_fleurs.svg"
+              alt="Image pot de fleurs"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <style>
+.imgSubtitle {
+  height: 1.5em;
+  width: 1.5em;
+}
+.subtitle {
+  color: #5c4725;
+  text-decoration: underline;
+}
+.firstLittleBee {
+  position: absolute;
+  left: 83vw;
+  top: 50vw;
+  transform: rotate(60deg);
+  width: 3em;
+}
+.secondeLittleBee {
+  position: absolute;
+  left: 6vw;
+  top: 61vw;
+  transform: rotate(-55deg);
+  width: 3em;
+}
+.imgFlowers {
+  object-fit: cover;
+  width: 100%;
+  height: 15vw;
+}
+@media (min-width: 500px) {
+  .firstLittleBee {
+    left: 85vw;
+    top: 30vw;
+  }
+  .secondeLittleBee {
+    left: 12vw;
+    top: 32vw;
+  }
+}
+@media (min-width: 800px) {
+  .firstLittleBee {
+    left: 85vw;
+    top: 10vw;
+  }
+  .secondeLittleBee {
+    left: 12vw;
+    top: 14vw;
+  }
+}
 </style>
