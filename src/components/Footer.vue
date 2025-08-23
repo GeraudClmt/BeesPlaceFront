aa846266788e4ad5f91c09e8395f47d8f3b3dc89
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-08-23 19:18:47 +0200

    Création du footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer class="footer">
+        <p>&copy; 2024 BeesPlace. Tous droits réservés.</p>
+    </footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.footer {
+    padding: 1rem;
+    background: #5C4725;
+    color: white;
+    font-size: 1rem;
+}
+</style>
