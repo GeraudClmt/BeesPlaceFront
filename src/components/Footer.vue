479e826b2eee46764d9de8d9e27b72041cff1841
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-08-24 14:53:34 +0000

    Correction de la date du footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,17 @@
 <template>
-    <footer class="footer">
-        <p>&copy; 2024 BeesPlace. Tous droits réservés.</p>
-    </footer>
+  <footer class="footer">
+    <p>&copy; 2025 BeesPlace. Tous droits réservés.</p>
+  </footer>
 </template>
 
 <script setup>
-
 </script>
 
 <style scoped>
 .footer {
-    padding: 1rem;
-    background: #5C4725;
-    color: white;
-    font-size: 1rem;
+  padding: 1rem;
+  background: #5c4725;
+  color: white;
+  font-size: 1rem;
 }
 </style>
