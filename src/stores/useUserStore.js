ba5eccc0fcd1673a9b4461d59b355ad3206b4bba
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-08-31 16:50:05 +0000

    Correction du store pinia pour le stockage de l'access token

--- src/stores/useUserStore.js
+++ src/stores/useUserStore.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('user', {
-    state: () => ({ token_access: "le token de test" }),
+    state: () => ({ token_access: null }),
     getters: {
         getToken: (state) => state.token_access,
     },
@@ -9,5 +9,8 @@ export const useUserStore = defineStore('user', {
         setToken(token_access) {
             this.token_access = token_access
         },
+        removeToken() {
+            this.token_access = null
+        }
     },
 })
