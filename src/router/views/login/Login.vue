e514d16bcf47089293643106622296035ae2d35c
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-08-31 16:48:35 +0000

    Gestion des codes retours de connexion, ajout de la redirection si connecté

--- src/router/views/login/Login.vue
+++ src/router/views/login/Login.vue
@@ -2,10 +2,15 @@
 import { useUserStore } from "@/stores/useUserStore";
 import Footer from "@/components/Footer.vue";
 import { ref } from "vue";
+import login from "@/services/login";
+import { useRouter } from "vue-router";
 
+const router = useRouter();
 const userStore = useUserStore();
 const email = ref();
 const password = ref();
+const isLoading = ref(false);
+const charginMessage = ref("Chargement en cours...");
 
 const rulesEmail = [
   (value) => !!value || "Required.",
@@ -16,6 +21,33 @@ const rulesPassword = [
   (value) => !!value || "Required.",
   (value) => (value && value.length >= 3) || "Min 8 characters",
 ];
+
+const connexion = () => {
+  isLoading.value = true;
+  charginMessage.value = "Chargement en cours...";
+
+  const fetchData = async () => {
+    try {
+      const response = await login.login(email.value, password.value);
+
+      userStore.setToken(response.data.token);
+      if (response.status === 200) {
+        charginMessage.value = "Connexion réussie";
+        setTimeout(() => {
+          router.push("/");
+        }, 3000);
+      }
+    } catch (error) {
+      charginMessage.value = error.response?.data?.message || "Erreur inconnue";
+    } finally {
+      setTimeout(() => {
+        isLoading.value = false;
+      }, 3000);
+    }
+  };
+
+  fetchData();
+};
 </script>
 
 <template>
@@ -28,7 +60,7 @@ const rulesPassword = [
       </v-col>
     </v-row>
     <v-row class="d-flex justify-center align-center text-center">
-      <v-col class="bgWhite elevation-5 pa-6" cols="11" md="6">
+      <v-col class="bgWhite elevation-5 pa-6" cols="11" md="4">
         <v-row>
           <h1>Connexion</h1>
         </v-row>
@@ -47,6 +79,7 @@ const rulesPassword = [
           <v-col>
             <v-text-field
               v-model="password"
+              type="password"
               :rules="rulesPassword"
               hide-details="auto"
               label="Mot de passe"
@@ -64,16 +97,33 @@ const rulesPassword = [
             <v-btn class="btnSecondary">Créer un compte</v-btn>
           </v-col>
           <v-col cols="6" class="shortPadding">
-            <v-btn class="btnPrimary">Se connecter</v-btn>
+            <v-btn
+              :disabled="!email || !password ? true : false"
+              class="btnPrimary"
+              @click="connexion"
+            >
+              Se connecter
+            </v-btn>
           </v-col>
         </v-row>
       </v-col>
     </v-row>
+
+    <div class="backGroundProgress" v-if="isLoading">
+      <v-progress-circular
+        class="progressCircular"
+        :size="70"
+        :width="7"
+        color="#5C4725"
+        indeterminate
+      ></v-progress-circular>
+      <p class="progressCircularText">{{ charginMessage }}</p>
+    </div>
   </v-container>
   <Footer />
 </template>
 
-<style>
+<style scooped>
 .linkForgetPwd {
   text-decoration: underline;
   color: black;
@@ -85,5 +135,27 @@ const rulesPassword = [
 
 .fullScreen {
   height: 94vh;
+}
+
+.progressCircular {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+.progressCircularText {
+  position: absolute;
+  left: 50%;
+  top: 58%;
+  transform: translate(-50%, 0%);
+}
+
+.backGroundProgress {
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: rgba(255, 246, 228, 0.9);
+  height: 100vh;
+  width: 100vw;
 }
 </style>
