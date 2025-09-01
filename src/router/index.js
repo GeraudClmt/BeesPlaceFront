302464b7293a814788a661fe9309afcd4e81140a
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-09-01 18:42:07 +0000

    Création des view register et forgot passord

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHashHistory } from "vue-router"
 import Accueil from "@/router/views/Index.vue"
 import Login from "@/router/views/login/Login.vue"
+import Register from '@/router/views/login/Register.vue'
+import ForgotPassword from '@/router/views/login/ForgotPassword.vue'
 
 const routes = [
     {
@@ -12,6 +14,16 @@ const routes = [
         path: '/login',
         name: 'Connexion',
         component: Login
+    },
+    {
+        path: '/register',
+        name: 'Creation de compte',
+        component: Register
+    },
+    {
+        path: '/forgot',
+        name: 'Récupération du mot de passe',
+        component: ForgotPassword
     }
 ]
 
